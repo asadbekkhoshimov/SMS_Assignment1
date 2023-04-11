3e5f1a8ee815f78b765bb738b99d13a07bf62a74
asadbekkhoshimov <asadbekbosss@gmail.com>
2023-04-11 16:17:43 +0000

    new

--- Homework3/catkin_ws/Homework3/build/Homework3/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/catkin_ws/Homework3/src/Homework3
-# Build directory: /home/ubuntu/catkin_ws/Homework3/build/Homework3
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
