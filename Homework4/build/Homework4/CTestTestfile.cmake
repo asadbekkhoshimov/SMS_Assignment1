6cf223ae892735f3d475776e411fc954fd74c076
asadbekkhoshimov <asadbekbosss@gmail.com>
2023-04-21 09:08:12 +0000

    adding Homework4

---
+++ Homework4/build/Homework4/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/SMS_Assignment1/Homework4/src/Homework4
+# Build directory: /home/ubuntu/SMS_Assignment1/Homework4/build/Homework4
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
