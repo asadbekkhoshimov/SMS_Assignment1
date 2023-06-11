368344fc83728fda1c90ba43780fce2ae0b39343
Asadbek_Khoshimov <84382619+asadbekkhoshimov@users.noreply.github.com>
2023-06-12 08:09:10 +0900

    Update city_publisher.py

--- BuddyBot/WeatherWise/src/weather_info/scripts/city_publisher.py
+++ BuddyBot/WeatherWise/src/weather_info/scripts/city_publisher.py
@@ -1,73 +1,18 @@
-import requests
+else:
+            print(f"\nCity Name: {resp.city_name}")
+            print(f"Temperature: {resp.temperature}°C")
+            print(f"Humidity: {resp.humidity}%")
+            print(f"Description: {resp.description}\n")
 
-def get_directions(destination):
-    base_url = "https://www.mapquestapi.com/directions/v2/route?"
-    api_key = "JAEpAr2sJYI7LMUM2pSxuXQjEFB1w5cl"
-    start_location = "Incheon"
+            # Define severe conditions
+            severe_conditions = ["Extreme", "Thunderstorm", "Heavy Rain"]
 
-    # Replace spaces in the destination with '+' for URL encoding
-    destination = destination.replace(" ", "+")
+            # Check if current weather matches any severe condition
+            if resp.description in severe_conditions:
+                print(f"Alert: Severe weather condition detected in {resp.city_name}!\n")
 
-    modes = ["fastest", "shortest", "bicycle", "pedestrian"]
-    mode_names = ["By Car (Fastest)", "By Car (Shortest Distance)", "By Bicycle", "On Foot"]
-    distances = []
-    times = []
-
-    for i, mode in enumerate(modes):
-        final_url = f"{base_url}key={api_key}&from={start_location}&to={destination}&routeType={mode}"
-
-        try:
-            response = requests.get(final_url)
-            data = response.json()
-
-            # Check if the request was successful
-            if response.status_code != 200:
-                print("Error: Unable to fetch directions. Please try again.")
-                return None, None, None
-
-            # Check if a route was found
-            if "route" not in data or "legs" not in data["route"] or len(data["route"]["legs"]) == 0:
-                print("Error: No route found to the specified destination.")
-                return None, None, None
-
-            # Get the distance and time from the response
-            distance = data["route"]["distance"] * 1.60934  # Convert miles to kilometers
-            time = data["route"]["time"]
-
-            distances.append(distance)
-            times.append(time)
-
-            # Print out the time and distance for each mode
-            print(f"{mode_names[i]}: Distance = {distance:.2f} km, Time = {time//3600} hours {(time%3600)//60} minutes")
-
-        except requests.exceptions.RequestException as e:
-            print(f"Error: An error occurred while fetching directions. {e}")
-            return None, None, None
-
-    # Determine the best mode of transportation based on distance
-    best_distance = min(distances)
-
-    if best_distance < 1:
-        best_mode = "On Foot"
-    elif best_distance < 5:
-        best_mode = "By Bicycle"
-    elif best_distance < 50:
-        best_mode = "By Car"
-    elif best_distance < 200:
-        best_mode = "By Train or Car"
-    else:
-        best_mode = "By Plane"
-
-    # Determine the time for the best mode of transportation
-    best_time = times[distances.index(best_distance)]
-    best_time_hours = best_time // 3600
-    best_time_minutes = (best_time % 3600) // 60
-
-    # Print out the best mode of transportation and time
-    print(f"\nThe best mode of transportation to {destination} is {best_mode}.")
-    print(f"Estimated travel time: {best_time_hours} hours {best_time_minutes} minutes.")
-
-    return mode_names, best_distance, f"{best_time_hours} hours {best_time_minutes} minutes"
+        # Sleep for 5 seconds
+        time.sleep(5)
 
 def main():
     while True:
@@ -79,28 +24,48 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == "1":
-            # Existing code for current weather
-            pass
+            city = input("Enter a city name: ")
+            resp = weather_client(city)
+
+            if resp.city_name == "Invalid city":
+                print("Invalid city")
+            else:
+                print(f"\nCity Name: {resp.city_name}")
+                print(f"Temperature: {resp.temperature}°C")
+                print(f"Humidity: {resp.humidity}%")
+                print(f"Description: {resp.description}\n")
+
         elif choice == "2":
-            # Existing code for travel distance
-            pass
+            city = input("Where do you want to travel? ")
+            resp = weather_client(city)
+
+            if resp.city_name == "Invalid city":
+                print("Invalid city")
+            else:
+                print(f"\nCity Name: {resp.city_name}")
+                print(f"Temperature: {resp.temperature}°C")
+                print(f"Humidity: {resp.humidity}%")
+                print(f"Description: {resp.description}\n")
+                print("Recommended Clothes: " + suggest_clothes(resp.temperature))
+
         elif choice == "3":
-            # Existing code for alerts/notifications
-            pass
+            city = input("Enter a city name for alerts: ")
+            weather_alerts(city)
+
         elif choice == "4":
             destination = input("Where would you like to travel to? ")
             directions, distance, time = get_directions(destination)
 
             if directions is not None and distance is not None and time is not None:
-                print(f"\nDistance: {distance:.2f} km")
+                print(f"\nDistance: {distance} miles")
                 print(f"Estimated Travel Time: {time}")
                 print("Directions:")
                 for direction in directions:
                     print(direction)
-        elif choice == "5":
+        elif choice == "4":
             break
         else:
-            print("Invalid choice. Please enter a number between 1 and 5.\n")
+            print("Invalid choice. Please enter a number between 1 and 4.\n")
 
-if __name__ == "__main__":
+if name == "__main__":
     main()
