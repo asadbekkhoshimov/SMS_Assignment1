cd660e01c3038b832846b03efa1f7522fee5a878
Asadbek_Khoshimov <84382619+asadbekkhoshimov@users.noreply.github.com>
2023-06-12 08:07:19 +0900

    Update city_publisher.py

--- BuddyBot/WeatherWise/src/weather_info/scripts/city_publisher.py
+++ BuddyBot/WeatherWise/src/weather_info/scripts/city_publisher.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python3
-import rospy
-from weather_info.srv import Weather
-import time
-import json
 import requests
 
 def get_directions(destination):
@@ -28,12 +23,12 @@ def get_directions(destination):
             # Check if the request was successful
             if response.status_code != 200:
                 print("Error: Unable to fetch directions. Please try again.")
-                return
+                return None, None, None
 
             # Check if a route was found
             if "route" not in data or "legs" not in data["route"] or len(data["route"]["legs"]) == 0:
                 print("Error: No route found to the specified destination.")
-                return
+                return None, None, None
 
             # Get the distance and time from the response
             distance = data["route"]["distance"] * 1.60934  # Convert miles to kilometers
@@ -47,7 +42,7 @@ def get_directions(destination):
 
         except requests.exceptions.RequestException as e:
             print(f"Error: An error occurred while fetching directions. {e}")
-            return
+            return None, None, None
 
     # Determine the best mode of transportation based on distance
     best_distance = min(distances)
@@ -72,62 +67,7 @@ def get_directions(destination):
     print(f"\nThe best mode of transportation to {destination} is {best_mode}.")
     print(f"Estimated travel time: {best_time_hours} hours {best_time_minutes} minutes.")
 
-def weather_client(city_name, service_name='weather_info'):
-    rospy.wait_for_service(service_name)
-    try:
-        weather_info = rospy.ServiceProxy(service_name, Weather)
-        resp = weather_info(city_name)
-        return resp
-    except rospy.ServiceException as e:
-        print("Service call failed: %s" % e)
-
-def suggest_clothes(temperature):
-    if temperature < 0:
-        return "Coat, Hat, Gloves, Warm Pants, Boots"
-    elif temperature < 10:
-        return "Jacket, Long Sleeve Shirt, Pants, Shoes"
-    elif temperature < 20:
-        return "Sweater, Jeans, Shoes"
-    elif temperature < 30:
-        return "T-Shirt, Shorts, Sandals"
-    else:
-        return "Tank Top, Shorts, Sandals"
-
-def check_severe_weather(resp):
-    if resp.description == "thunderstorm":
-        return "Alert: Thunderstorm in the area!"
-    elif resp.description == "heavy snow":
-        return "Alert: Heavy snowfall in the area!"
-    elif resp.temperature < -10:
-        return "Alert: Extremely cold temperature!"
-    elif resp.temperature > 40:
-        return "Alert: Extremely hot temperature!"
-    else:
-        return "Weather is normal"
-
-def weather_alerts(city):
-    while True:
-        resp = weather_client(city)
-
-        if resp.city_name == "Invalid city":
-            print("Invalid city")
-            return
-
-        else:
-            print(f"\nCity Name: {resp.city_name}")
-            print(f"Temperature: {resp.temperature}°C")
-            print(f"Humidity: {resp.humidity}%")
-            print(f"Description: {resp.description}\n")
-
-            # Define severe conditions
-            severe_conditions = ["Extreme", "Thunderstorm", "Heavy Rain"]
-
-            # Check if current weather matches any severe condition
-            if resp.description in severe_conditions:
-                print(f"Alert: Severe weather condition detected in {resp.city_name}!\n")
-
-        # Sleep for 5 seconds
-        time.sleep(5)
+    return mode_names, best_distance, f"{best_time_hours} hours {best_time_minutes} minutes"
 
 def main():
     while True:
@@ -139,48 +79,28 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == "1":
-            city = input("Enter a city name: ")
-            resp = weather_client(city)
-
-            if resp.city_name == "Invalid city":
-                print("Invalid city")
-            else:
-                print(f"\nCity Name: {resp.city_name}")
-                print(f"Temperature: {resp.temperature}°C")
-                print(f"Humidity: {resp.humidity}%")
-                print(f"Description: {resp.description}\n")
-
+            # Existing code for current weather
+            pass
         elif choice == "2":
-            city = input("Where do you want to travel? ")
-            resp = weather_client(city)
-
-            if resp.city_name == "Invalid city":
-                print("Invalid city")
-            else:
-                print(f"\nCity Name: {resp.city_name}")
-                print(f"Temperature: {resp.temperature}°C")
-                print(f"Humidity: {resp.humidity}%")
-                print(f"Description: {resp.description}\n")
-                print("Recommended Clothes: " + suggest_clothes(resp.temperature))
-
+            # Existing code for travel distance
+            pass
         elif choice == "3":
-            city = input("Enter a city name for alerts: ")
-            weather_alerts(city)
-
+            # Existing code for alerts/notifications
+            pass
         elif choice == "4":
             destination = input("Where would you like to travel to? ")
             directions, distance, time = get_directions(destination)
 
             if directions is not None and distance is not None and time is not None:
-                print(f"\nDistance: {distance} miles")
+                print(f"\nDistance: {distance:.2f} km")
                 print(f"Estimated Travel Time: {time}")
                 print("Directions:")
                 for direction in directions:
                     print(direction)
-        elif choice == "4":
+        elif choice == "5":
             break
         else:
-            print("Invalid choice. Please enter a number between 1 and 4.\n")
+            print("Invalid choice. Please enter a number between 1 and 5.\n")
 
 if __name__ == "__main__":
     main()
