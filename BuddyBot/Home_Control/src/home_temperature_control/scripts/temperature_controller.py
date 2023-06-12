edc93720c6ce290f187ccf1b538d51bfe069abfa
Asadbek_Khoshimov <84382619+asadbekkhoshimov@users.noreply.github.com>
2023-06-12 20:57:58 +0900

    Update temperature_controller.py

--- BuddyBot/Home_Control/src/home_temperature_control/scripts/temperature_controller.py
+++ BuddyBot/Home_Control/src/home_temperature_control/scripts/temperature_controller.py
@@ -1,58 +1,34 @@
-#!/usr/bin/env python3
-import rospy
-from std_msgs.msg import Float32
-import time
-import random
+def control_temperature(self):
+    if self.current_temp is None:
+        return
 
-class TemperatureController:
-    def __init__(self):
-        self.current_temp = None
-        self.target_temp = None
-        self.temperature_change = 0  # Change in temperature per second
-        self.mode = None  # Operation mode: 'manual' or 'automatic'
-        rospy.init_node('temperature_controller', anonymous=True)
-        rospy.Subscriber('room_temperature', Float32, self.temp_callback)
+    if self.mode == 'manual':
+        print("Choose a system:")
+        print("1. Air Conditioner")
+        print("2. Heating System")
+        system_choice = int(input("Your choice (1-2): "))
 
-    def temp_callback(self, data):
-        self.current_temp = data.data
-        self.control_temperature()
-
-    def control_temperature(self):
-        if self.current_temp is None:
-            return
-        if self.mode == 'manual':
-            command = input("Enter command (ac/heating on/off): ")
-            if command == 'ac on':
+        if system_choice == 1:
+            print("Air Conditioner:")
+            print("1. On")
+            print("2. Off")
+            ac_choice = int(input("Your choice (1-2): "))
+            if ac_choice == 1:
                 self.temperature_change = -0.8
-            elif command == 'ac off' or command == 'heating off':
+            elif ac_choice == 2:
                 self.temperature_change = 0
                 print(f"Current room temperature: {self.current_temp} degree Celsius")
-            elif command == 'heating on':
+
+        elif system_choice == 2:
+            print("Heating System:")
+            print("1. On")
+            print("2. Off")
+            heating_choice = int(input("Your choice (1-2): "))
+            if heating_choice == 1:
                 self.temperature_change = 0.8
-        elif self.mode == 'automatic':
-            if self.current_temp < self.target_temp - 3:
-                print("Turning on heating system...")
-                self.temperature_change = 0.8
-            elif self.current_temp > self.target_temp + 3:
-                print("Turning on air conditioner...")
-                self.temperature_change = -0.8
-            else:
+            elif heating_choice == 2:
                 self.temperature_change = 0
-                print("Maintaining current temperature...")
-                if self.current_temp == self.target_temp:
-                    self.current_temp += random.uniform(-3, 3)
+                print(f"Current room temperature: {self.current_temp} degree Celsius")
 
-        self.adjust_temperature()
-
-    def adjust_temperature(self):
-        time.sleep(1)  # Wait for 1 second
-        self.current_temp += self.temperature_change
-        print(f"Current temperature: {self.current_temp} degree Celsius")
-
-if __name__ == '__main__':
-    controller = TemperatureController()
-    while True:
-        controller.mode = input("Enter operation mode (manual/automatic): ")
-        if controller.mode == 'automatic':
-            controller.target_temp = float(input("Enter desired room temperature: "))
-        rospy.spin()
+    elif self.mode == 'automatic':
+        # rest of the code...
