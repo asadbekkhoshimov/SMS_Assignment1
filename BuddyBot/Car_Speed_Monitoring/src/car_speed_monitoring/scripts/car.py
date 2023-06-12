2b94d29ec327ceaf95361aae3ac132f38bf2d9e4
Asadbek_Khoshimov <84382619+asadbekkhoshimov@users.noreply.github.com>
2023-06-12 10:38:46 +0900

    Update car.py

--- BuddyBot/Car_Speed_Monitoring/src/car_speed_monitoring/scripts/car.py
+++ BuddyBot/Car_Speed_Monitoring/src/car_speed_monitoring/scripts/car.py
@@ -8,16 +8,16 @@ import threading
 
 class Car:
     def __init__(self):
-        self.base_speed = 0
+        self.base_speed = random.randint(100, 200)  # random base speed between 100 and 200 km/h
         self.speed = self.base_speed
-        self.max_speed = 0
-        self.min_speed = float('inf')
-        self.speed_sum = 0
-        self.speed_count = 0
         self.distance = random.randint(30, 60)  # random distance in km
-        self.travel_time = 30  # time in seconds
-        self.weather_conditions = set()
         self.city = input('Where are you traveling? ')
+        self.weather = ""
+        self.min_speed = self.base_speed
+        self.max_speed = self.base_speed
+        self.total_speed = 0
+        self.speed_readings = 0
+        self.weather_conditions = set()
 
         # Print initial distance and destination
         print(f'Travelling to {self.city}, initial distance: {self.distance} km')
@@ -28,41 +28,29 @@ class Car:
 
     def adjust_speed(self, weather):
         self.weather_conditions.add(weather.data)
-        if weather.data == 'sunny':
-            self.base_speed = self.distance * 3600 / self.travel_time
-        elif weather.data == 'rainy':
-            self.base_speed = self.distance * 3600 / self.travel_time * 0.75
-        elif weather.data == 'snowy':
-            self.base_speed = self.distance * 3600 / self.travel_time * 0.625
-        elif weather.data == 'windy':
-            self.base_speed = self.distance * 3600 / self.travel_time * 0.875
-        else:
-            self.base_speed = self.distance * 3600 / self.travel_time
-
-        print(f'Base speed adjusted to: {self.base_speed} km/h due to {weather.data} weather')
 
     def monitor_speed(self):
-        while not rospy.is_shutdown():
-            if self.speed > 0:
-                self.distance -= self.speed / 3600  # decrease the distance based on speed and time
-                self.speed = random.randint(int(self.base_speed*0.9), int(self.base_speed*1.1))
-                self.speed_sum += self.speed
-                self.speed_count += 1
-                self.max_speed = max(self.max_speed, self.speed)
-                self.min_speed = min(self.min_speed, self.speed)
-                warning_msg = ""
-                if self.weather_conditions - set(['sunny', '']):
-                    warning_msg = "Warning: Please drive slowly due to bad weather conditions."
-                
-                print(f'Travelling to {self.city}, total distance="60km", remain="{int(self.distance)} km", \ncurrent_speed="{self.speed} km/h", weather_condition="{self.weather_conditions}", \n{warning_msg}')
-                if self.distance <= 0:
-                    print('Arrived at destination')
-                    print(f'Encountered weather conditions: {self.weather_conditions}')
-                    print(f'Top speed: {self.max_speed} km/h')
-                    print(f'Minimum speed: {self.min_speed} km/h')
-                    print(f'Average speed: {self.speed_sum / self.speed_count} km/h')
-                    rospy.signal_shutdown('Arrival')
+        start_time = time.time()
+        while not rospy.is_shutdown() and time.time() - start_time < 30:
+            self.speed = random.randint(self.base_speed - 10, self.base_speed + 10)
+            self.distance -= self.speed / 3600  # decrease the distance based on speed and time
+            self.min_speed = min(self.min_speed, self.speed)
+            self.max_speed = max(self.max_speed, self.speed)
+            self.total_speed += self.speed
+            self.speed_readings += 1
+            
+            warning_msg = ""
+            if self.weather in ['rainy', 'snowy', 'windy']:
+                warning_msg = "Warning: Please drive slowly due to bad weather conditions."
+
+            print(f'Travelling to {self.city}, total distance="60km", remain="{int(self.distance)} km", \ncurrent_speed="{self.speed} km/h", weather_condition="{self.weather}", \n{warning_msg}')
             time.sleep(1)
+        
+        print("30 seconds have passed!")
+        print(f"Minimum Speed: {self.min_speed} km/h")
+        print(f"Maximum Speed: {self.max_speed} km/h")
+        print(f"Average Speed: {self.total_speed/self.speed_readings} km/h")
+        print(f"Weather Conditions Encountered: {self.weather_conditions}")
 
 
 def listener():
