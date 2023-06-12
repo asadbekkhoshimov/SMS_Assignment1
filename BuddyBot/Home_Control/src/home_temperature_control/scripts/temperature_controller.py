763c80999bf6287d8dd9a2546f58a320e5152519
Asadbek_Khoshimov <84382619+asadbekkhoshimov@users.noreply.github.com>
2023-06-12 21:09:04 +0900

    Update temperature_controller.py

--- BuddyBot/Home_Control/src/home_temperature_control/scripts/temperature_controller.py
+++ BuddyBot/Home_Control/src/home_temperature_control/scripts/temperature_controller.py
@@ -17,31 +17,32 @@ class TemperatureController:
     def control_temperature(self):
         if self.current_temp is None:
             return
+
         if self.mode == 'manual':
             print("Choose a system:")
             print("1. Air Conditioner")
             print("2. Heating System")
-            system_choice = int(input("Your choice (1-2): "))
+            system_choice = input("Your choice (1-2): ")
 
-        if system_choice == 1:
-            print("Air Conditioner:")
-            print("1. On")
-            print("2. Off")
-            ac_choice = int(input("Your choice (1-2): "))
-            if ac_choice == 1:
-                self.temperature_change = -0.8
-            elif ac_choice == 2:
-                self.temperature_change = 0
-                print(f"Current room temperature: {self.current_temp} degree Celsius")
+            if system_choice == '1':
+                print("Air Conditioner:")
+                print("1. On")
+                print("2. Off")
+                ac_choice = input("Your choice (on/off): ")
+                if ac_choice.lower() == 'on':
+                    self.temperature_change = -0.8
+                elif ac_choice.lower() == 'off':
+                    self.temperature_change = 0
+                    print(f"Current room temperature: {self.current_temp} degree Celsius")
 
-        elif system_choice == 2:
+        elif system_choice == '2':
             print("Heating System:")
             print("1. On")
             print("2. Off")
-            heating_choice = int(input("Your choice (1-2): "))
-            if heating_choice == 1:
+            heating_choice = input("Your choice (on/off): ")
+            if heating_choice.lower() == 'on':
                 self.temperature_change = 0.8
-            elif heating_choice == 2:
+            elif heating_choice.lower() == 'off':
                 self.temperature_change = 0
                 print(f"Current room temperature: {self.current_temp} degree Celsius")
                 
